f380e3a7a98c92a003895e67294e20004da035d5
Mark YF Fang <markfang1030@gmail.com>
2016-01-04 08:51:54 -0500

    Remove EJS

--- TruthOrDareServer.js
+++ TruthOrDareServer.js
@@ -9,27 +9,27 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-    var tmpTask = '';
-    var connection = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'truthordare',
-        password : 'JackAndMark'
-    });
-
-    connection.connect();
-    connection.query('SELECT Content from TruthGame.Dare where TaskID=45;', function(err, rows, fields) {
-        connection.end(function(err) {console.log('connection ended')});
-        if (err) throw err;
-        tmpTask = rows[0].Content;
-        console.log(rows[0].Content);
-        console.log(tmpTask);
-        res.render('index', { content: tmpTask });
-    });
-});
+//app.get('/', function(req, res) {
+//    var tmpTask = '';
+//    var connection = mysql.createConnection({
+//        host     : 'localhost',
+//        user     : 'truthordare',
+//        password : 'JackAndMark'
+//    });
+//
+//    connection.connect();
+//    connection.query('SELECT Content from TruthGame.Dare where TaskID=45;', function(err, rows, fields) {
+//        connection.end(function(err) {console.log('connection ended')});
+//        if (err) throw err;
+//        tmpTask = rows[0].Content;
+//        console.log(rows[0].Content);
+//        console.log(tmpTask);
+//        res.render('index', { content: tmpTask });
+//    });
+//});
 
 app.post('/getTask', function(req, res) {
     var queryType = 0;
